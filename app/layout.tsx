32c9a4a1be2be939bcdb002ccd0e9b7d95412cd1
abubakarsiddiquii <abubakarsiddiqui537@gmail.com>
2026-04-09 08:01:23 +0530

    interview generation enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 
-import "./globals.css";
+import "./globals.css"
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
