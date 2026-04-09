d3edcebc02f4b55455cec2dc1748740eb65facab
abubakarsiddiquii <abubakarsiddiqui537@gmail.com>
2026-04-09 09:26:57 +0530

    enhanced UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -67,7 +67,7 @@ async function Home() {
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
-        <h2>Take an Interviews</h2>
+        <h2>Take an Interview</h2>
 
         <div className="interviews-section">
           {hasUpcomingInterviews ? (
